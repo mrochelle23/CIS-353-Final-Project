765d7311e6576892cffadb7853c48e786bc46e69
ksanders05 <captinkis@gmail.com>
2026-04-19 15:56:51 -0400

    Create sanders_finalproject.sql

---
+++ sanders_finalproject.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Camera (
+    camera_id INT PRIMARY KEY,
+    camera_model VARCHAR(100),
+    install_date DATE
+);
+
+CREATE TABLE Environment (
+    environment_id INT PRIMARY KEY,
+    description TEXT
+);
+
+CREATE TABLE Location (
+    location_id INT,
+    location_name VARCHAR(100),
+    camera_id INT NOT NULL,
+    environment_id INT,
+    latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6),
+    PRIMARY KEY (location_id, location_name),
+    FOREIGN KEY (camera_id) REFERENCES Camera(camera_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (environment_id) REFERENCES Environment(environment_id)
+);
