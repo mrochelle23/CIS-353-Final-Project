3a8b614ff62b79610c012fa418f218676c32f7e8
mrochelle23 <mrochellebiz@gmail.com>
2026-04-19 23:35:24 -0400

    fixed a spelling error in the new comment

--- mikhai-create_schema.sql
+++ mikhai-create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE Species {
 }
 
 -- Creating the OBSERVATION table
--- Weak entity dependet on Image and Species
+-- Weak entity dependent on Image and Species
 CREATE TABLE Observation {
     image_id INT,
     observation_num INT,
