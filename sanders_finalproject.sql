93e522e8300ab9aff2c6d6cbf66d81e0d54b095d
ksanders05 <captinkis@gmail.com>
2026-04-21 11:58:08 -0400

    Update sanders_finalproject.sql

--- sanders_finalproject.sql
+++ sanders_finalproject.sql
@@ -1,3 +1,4 @@
+-- Create tables
 CREATE TABLE Environment (
     environment_id INT PRIMARY KEY,
     description TEXT
@@ -15,14 +16,13 @@ CREATE TABLE Location (
 );
 
 -- Populate tables
-INSERT INTO Environment (environment_id, description)
+--INSERT INTO Environment (environment_id, description), here for reference
+INSERT INTO Environment VALUES(201, )
+INSERT INTO Environment VALUES(202, )
+INSERT INTO Environment VALUES(203, )
 
-INSERT INTO Environment (environment_id, description)
-
-INSERT INTO Environment (environment_id, description)
-
-INSERT INTO Location (location_id, location_name, camera_id, environment_id)
-
-INSERT INTO Location (location_id, location_name, camera_id, environment_id)
-
-INSERT INTO Location (location_id, location_name, camera_id, environment_id)
+-- INSERT INTO Location (location_id, location_name, camera_id, environment_id)
+INSERT INTO Location VALUES(301, , ,)
+INSERT INTO Location VALUES(302, , ,)
+INSERT INTO Location VALUES(303, , ,)
+-- Queries
