6c8187802628012a3fedb1f40aaef860c789ac2f
ksanders05 <captinkis@gmail.com>
2026-04-21 11:38:43 -0400

    Update sanders_finalproject.sql

--- sanders_finalproject.sql
+++ sanders_finalproject.sql
@@ -8,10 +8,21 @@ CREATE TABLE Location (
     location_name VARCHAR(100),
     camera_id INT NOT NULL,
     environment_id INT,
-    latitude DECIMAL(9,6),
-    longitude DECIMAL(9,6),
     PRIMARY KEY (location_id, location_name),
     FOREIGN KEY (camera_id) REFERENCES Camera(camera_id)
         ON DELETE CASCADE,
     FOREIGN KEY (environment_id) REFERENCES Environment(environment_id)
 );
+
+-- Populate tables
+INSERT INTO Environment (environment_id, description)
+
+INSERT INTO Environment (environment_id, description)
+
+INSERT INTO Environment (environment_id, description)
+
+INSERT INTO Location (location_id, location_name, camera_id, environment_id)
+
+INSERT INTO Location (location_id, location_name, camera_id, environment_id)
+
+INSERT INTO Location (location_id, location_name, camera_id, environment_id)
