57bd60d1b5af0aa2b05143df3d1432ca810cbb45
ksenci8 <ksencilo88@gmail.com>
2026-04-22 14:16:46 -0400

    query 1 corrected and works

--- mikhai_tables.sql
+++ mikhai_tables.sql
@@ -1,28 +1,3 @@
--- Prerequisite: Camera and Image must already exist before this script runs.
--- Observation connects to Camera indirectly through Image:
--- Observation.image_id -> Image.image_id -> Image.camera_id -> Camera.camera_id
-
-BEGIN
-    EXECUTE IMMEDIATE 'DROP TABLE Observation CASCADE CONSTRAINTS';
-EXCEPTION
-    WHEN OTHERS THEN
-        IF SQLCODE != -942 THEN
-            RAISE;
-        END IF;
-END;
-/
-
-BEGIN
-    EXECUTE IMMEDIATE 'DROP TABLE Species CASCADE CONSTRAINTS';
-EXCEPTION
-    WHEN OTHERS THEN
-        IF SQLCODE != -942 THEN
-            RAISE;
-        END IF;
-END;
-/
-
--- Create Species table
 CREATE TABLE Species (
     species_id NUMBER PRIMARY KEY,
     common_name VARCHAR2(100) NOT NULL,
@@ -30,10 +5,8 @@ CREATE TABLE Species (
     conservation_status VARCHAR2(50)
 );
 
--- Create Observation table
--- Observation depends on Image and Species
 CREATE TABLE Observation (
-    image_id NUMBER NOT NULL,
+    image_id NUMBER,
     observation_num NUMBER,
     species_count NUMBER NOT NULL CHECK (species_count > 0),
     behavior_type VARCHAR2(100),
@@ -48,7 +21,8 @@ CREATE TABLE Observation (
         REFERENCES Species(species_id)
 );
 
--- Populate Species with data
+
+-- Populating Species data
 INSERT INTO Species (species_id, common_name, scientific_name, conservation_status)
 VALUES (501, 'White-tailed Deer', 'Odocoileus virginianus', 'Least Concern');
 
@@ -58,8 +32,9 @@ VALUES (502, 'Coyote', 'Canis latrans', 'Least Concern');
 INSERT INTO Species (species_id, common_name, scientific_name, conservation_status)
 VALUES (503, 'Gray Wolf', 'Canis lupus', 'Endangered');
 
--- Populate Observation with data
--- image_id values must already exist in the Image table
+
+-- Populating Observation data
+-- Multiple observations can exist for a single image
 INSERT INTO Observation (image_id, observation_num, species_count, behavior_type, species_id)
 VALUES (101, 1, 2, 'Foraging', 501);
 
@@ -69,9 +44,11 @@ VALUES (101, 2, 1, 'Running', 502);
 INSERT INTO Observation (image_id, observation_num, species_count, behavior_type, species_id)
 VALUES (103, 1, 1, 'Resting', 501);
 
+-- adds another observation for the same image and species, but a 
+-- different observation num
+INSERT INTO Observation (image_id, observation_num, species_count, behavior_type, species_id)
+VALUES (103, 2, 2, 'Walking', 501);
+
+
 INSERT INTO Observation (image_id, observation_num, species_count, behavior_type, species_id)
 VALUES (104, 1, 3, 'Socializing', 503);
-
--- Verify loaded data
-SELECT * FROM Species;
-SELECT * FROM Observation;
