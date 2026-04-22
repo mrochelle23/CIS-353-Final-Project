910152878e505061dab78fcf9349d8cfba803140
ksenci8 <ksencilo88@gmail.com>
2026-04-22 14:00:34 -0400

    select * from statements tested and work as expected

---
+++ select-from.sql
@@ -0,0 +1,18 @@
+-- 1. Prints all data from Camera table
+SELECT * FROM Camera;
+
+-- 2. Prints all data from Image table
+SELECT * FROM Image;
+
+-- 3. Prints all data from Species table
+SELECT * FROM Species;
+
+-- 4. Print all data from Observation table
+SELECT * FROM Observation;
+
+-- 5. Prints all data from Location table
+SELECT * FROM Location;
+
+-- 6. Prints all data from Environment table
+SELECT * FROM Environment;
+
